d0d07ded3539235135aff7e010fd00e7e3ab459f
ander <smith@e-cyber.info>
2016-03-19 09:43:16 +0900

    タイトルバーを削除。

--- hello/app/src/main/java/com/example/ander/hello/MainActivity.java
+++ hello/app/src/main/java/com/example/ander/hello/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ander.hello;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
@@ -7,15 +8,23 @@ import android.opengl.GLSurfaceView;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLSurfaceView.Renderer;
+import android.view.Window;
+import android.view.WindowManager;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends /*AppCompat*/Activity {
 
     MyGLView myGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+      //  requestWindowFeature(Window.FEATURE_NO_TITLE);
+       getWindow().addFlags(
+               WindowManager.LayoutParams.FLAG_FULLSCREEN
+       );
+
         myGLView = new MyGLView(this);
         setContentView(myGLView);
     }
